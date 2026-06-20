bfe9c0f7610c5b0e824ef4aab6223739e5d53364
bgk614 <bgk614@gmail.com>
2026-06-20 14:43:37 +0900

    chore: setup test

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 @Injectable()
 export class AppService {
+  async getMessage() {
+    const filePath = join(process.cwd(), 'data', 'test.txt');
+
+    const content = await readFile(filePath, 'utf-8');
+
+    return {
+      message: content,
+    };
+  }
   getHello(): string {
     return 'Hello World!';
   }
