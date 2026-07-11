3365c32edfce87af3cb2a507952ad961b2407a10
bgk614 <bgk614@gmail.com>
2026-07-11 14:53:03 +0900

    chore: validation 수정

--- apps/api/src/dtos/user.dto.ts
+++ apps/api/src/dtos/user.dto.ts
@@ -1,10 +1,14 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @IsNotEmpty()
+  @IsString()
   nickname: string;
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser());
 
   const configService = app.get(ConfigService);
